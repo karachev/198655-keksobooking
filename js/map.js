597c105165c90b60bdfe3415c1a9092408bdd93c
Pavel Karachev <pavelkarachev@ymail.com>
2017-09-05 22:52:47 +0300

    fix git

--- js/map.js
+++ js/map.js
@@ -2,32 +2,18 @@
 
 window.pinSet();
 
-<<<<<<< HEAD
 var pinHandle = document.querySelector('.pin__main');
 var address = document.querySelector('#address');
 address.setAttribute('readonly', 'readonly');
 pinHandle.setAttribute('draggable', true);
-=======
 
->>>>>>> 051aa073c825eb2de15928bbf60d819f9d79460d
-
-
-<<<<<<< HEAD
 
   var startCoords = {
     x: evt.clientX,
     y: evt.clientY
   };
-=======
 
->>>>>>> 051aa073c825eb2de15928bbf60d819f9d79460d
-
-
-
-<<<<<<< HEAD
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 });
-=======
 
->>>>>>> 051aa073c825eb2de15928bbf60d819f9d79460d
