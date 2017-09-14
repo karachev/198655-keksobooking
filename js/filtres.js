a502fb7aacc559f930601f6f160d223807b125f1
Pavel Karachev <pavelkarachev@ymail.com>
2017-09-14 19:07:07 +0300

    change filter file

--- js/filtres.js
+++ js/filtres.js
@@ -1,19 +1,19 @@
 'use strict';
 
 (function () {
-  var filtersContainer = document.querySelector('.tokyo__filters');
-  var housesTypeElement = filtersContainer.querySelector('#housing_type');
-  var housesPriceElement = filtersContainer.querySelector('#housing_price');
-  var housesRoomCountElement = filtersContainer.querySelector('#housing_room-number');
-  var housesGuestCountElement = filtersContainer.querySelector('#housing_guests-number');
-  var featuresElements = filtersContainer.querySelectorAll('input[name="feature"]');
+  var filterBlock = document.querySelector('.tokyo__filters');
+  var typeFilter = filterBlock.querySelector('#housing_type');
+  var priceFilter = filterBlock.querySelector('#housing_price');
+  var roomFilter = filterBlock.querySelector('#housing_room-number');
+  var guestsFilter = filterBlock.querySelector('#housing_guests-number');
+  var featuresFilter = filterBlock.querySelectorAll('input[name="feature"]');
 
-  function setFilterByHousesTypes(filterValue, itemValue) {
+  function setFilterTypes(filterValue, itemValue) {
     return filterValue === 'any' || itemValue === filterValue;
   }
 
-  function setFilterByHousesPrice(price) {
-    var currentValue = housesPriceElement.value;
+  function setFilterPrice(price) {
+    var currentValue = priceFilter.value;
     switch (currentValue) {
       case 'middle':
         return price >= 10000 && price < 50000;
@@ -28,44 +28,32 @@
 
   function setFilterByHousesFeautures(filterFeatures, itemFeatures) {
     for (var i = 0; i < filterFeatures.length; i++) {
-      if (itemFeatures.indexOf(filterFeatures[i]) === -1) {
-        return false;
-      }
+      if (itemFeatures.indexOf(filterFeatures[i]) === -1) return false;
     }
     return true;
   }
 
   function setFilters() {
-    var hausesFeautures = [].filter.call(featuresElements, function (item) {
+    var hausesFeautures = [].filter.call(featuresFilter, function (item) {
       return item.checked;
     }).map(function (item) {
       return item.value;
     });
 
     return window.map.allOffers.filter(function (item) {
-      if (!setFilterByHousesTypes(housesTypeElement.value, item.offer.type)) {
-        return false;
-      }
-      if (!setFilterByHousesPrice(item.offer.price)) {
-        return false;
-      }
-      if (!setFilterByHousesTypes(housesRoomCountElement.value, item.offer.rooms + '')) {
-        return false;
-      }
-      if (!setFilterByHousesTypes(housesGuestCountElement.value, item.offer.guests + '')) {
-        return false;
-      }
-      if (!setFilterByHousesFeautures(hausesFeautures, item.offer.features)) {
-        return false;
-      }
+      if (!setFilterTypes(typeFilter.value, item.offer.type)) return false;
+      if (!setFilterPrice(item.offer.price)) return false;
+      if (!setFilterTypes(roomFilter.value, item.offer.rooms + '')) return false;
+      if (!setFilterTypes(guestsFilter.value, item.offer.guests + '')) return false;
+      if (!setFilterByHousesFeautures(hausesFeautures, item.offer.features)) return false;
       return true;
     });
   }
 
   window.filters = {
     setFilters: setFilters,
-    setFilterByHousesTypes: setFilterByHousesTypes,
-    setFilterByHousesPrice: setFilterByHousesPrice,
+    setFilterTypes: setFilterTypes,
+    setFilterPrice: setFilterPrice,
     setFilterByHousesFeautures: setFilterByHousesFeautures
   };
 })();
