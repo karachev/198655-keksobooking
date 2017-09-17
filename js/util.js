eaf76c4321582e7fa1ae90564719c5ec3498d13d
Pavel Karachev <pavelkarachev@ymail.com>
2017-09-17 21:17:54 +0300

    func window.util

--- js/util.js
+++ js/util.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.util = (function () {
+(function () {
   var ESCAPE_KEY_CODE = 27;
   var ENTER_KEY_CODE = 13;
   var TYPE = ['flat', 'house', 'bungalo', 'palace'];
@@ -10,42 +10,55 @@ window.util = (function () {
     '14:00'
   ];
   var pinMap = document.querySelector('.tokyo__pin-map');
+  function getRandomUniqueItem(array) {
+    var newArray = window.util.getRefreshArray(array);
+    return newArray.splice(window.util.getRandomNumber(0, newArray.length - 1), 1);
+  }
+  function getRandomUniqueItem(array) {
+    var newArray = window.util.getRefreshArray(array);
+    return newArray.splice(window.util.getRandomNumber(0, newArray.length - 1), 1);
+  }
+  function getRefreshArray(array) {
+    var m = array.length;
+    var t;
+    var i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
+  function getRandomNumber(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  function getRandomOfSet(minValue, maxValue) {
+    return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+  }
+  function isEscapePressed(evt) {
+    return evt && evt.keyCode === ESCAPE_KEY_CODE;
+  }
+  function isEnterPressed(evt) {
+    return evt && evt.keyCode === ENTER_KEY_CODE;
+  }
+  function isClicked(evt) {
+    return evt.type === 'click';
+  }
 
-  return {
-    pinMap: pinMap,
+  window.util= {
+    ESCAPE_KEY_CODE: ESCAPE_KEY_CODE,
+    ENTER_KEY_CODE: ENTER_KEY_CODE,
     TYPE: TYPE,
     CHECKIN: CHECKIN,
-
-    getRandomUniqueItem: function (array) {
-      var newArray = window.util.getRefreshArray(array);
-      return newArray.splice(window.util.getRandomNumber(0, newArray.length - 1), 1);
-    },
-    getRefreshArray: function (array) {
-      var m = array.length;
-      var t;
-      var i;
-      while (m) {
-        i = Math.floor(Math.random() * m--);
-        t = array[m];
-        array[m] = array[i];
-        array[i] = t;
-      }
-      return array;
-    },
-    getRandomNumber: function (array) {
-      return array[Math.floor(Math.random() * array.length)];
-    },
-    getRandomOfSet: function (minValue, maxValue) {
-      return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
-    },
-    isEscapePressed: function (evt) {
-      return evt && evt.keyCode === ESCAPE_KEY_CODE;
-    },
-    isEnterPressed: function (evt) {
-      return evt && evt.keyCode === ENTER_KEY_CODE;
-    },
-    isClicked: function (evt) {
-      return evt.type === 'click';
-    }
+    pinMap: pinMap,
+    getRandomUniqueItem: getRandomUniqueItem,
+    getRandomUniqueItem: getRandomUniqueItem,
+    getRefreshArray: getRefreshArray,
+    getRandomNumber: getRandomNumber,
+    getRandomOfSet: getRandomOfSet,
+    isEscapePressed: isEscapePressed,
+    isEnterPressed: isEnterPressed,
+    isClicked: isClicked
   };
 })();
